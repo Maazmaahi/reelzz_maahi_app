da3e1971980c27805036b81b9a4a2259462a9f64
Md Maaz Ahmad <nehalmaaz@gmail.com>
2025-02-05 02:17:31 -0800

    Pick and upload reel completed

--- src/navigation/MainNavigatior.tsx
+++ src/navigation/MainNavigatior.tsx
@@ -2,25 +2,28 @@ import {View, Text} from 'react-native';
 import React, {FC} from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {mergedStack} from './ScreenCollection';
-import { RoutesName } from '../constants/RoutesName';
+import {RoutesName} from '../constants/RoutesName';
+import {UploadProvider} from '../components/uploadservice/UploadContext';
 
 const Stack = createNativeStackNavigator();
 
 const MainNavigatior: FC = () => {
   return (
-    <Stack.Navigator
-      initialRouteName={RoutesName.Splash}
-      screenOptions={() => ({headerShown: false})}>
-      {mergedStack.map((item, index) => {
-        return (
-          <Stack.Screen
-            key={index}
-            name={item.name}
-            component={item.component}
-          />
-        );
-      })}
-    </Stack.Navigator>
+    <UploadProvider>
+      <Stack.Navigator
+        initialRouteName={RoutesName.Splash}
+        screenOptions={() => ({headerShown: false})}>
+        {mergedStack.map((item, index) => {
+          return (
+            <Stack.Screen
+              key={index}
+              name={item.name}
+              component={item.component}
+            />
+          );
+        })}
+      </Stack.Navigator>
+    </UploadProvider>
   );
 };
 
