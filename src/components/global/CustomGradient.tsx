bc5335bcae16d43d9873f39cb751452a81420c16
Md Maaz Ahmad <nehalmaaz@gmail.com>
2025-02-08 23:07:20 -0800

    Video playing initiated

--- src/components/global/CustomGradient.tsx
+++ src/components/global/CustomGradient.tsx
@@ -14,8 +14,8 @@ const CustomGradient: React.FC<CustomGradientProps> = ({
   const darkColors = [
     'rgba(0,0,0,0.9)',
     'rgba(0,0,0,0.8)',
-    'rgba(0,0,0,0.8)',
     'rgba(0,0,0,0.7)',
+    'rgba(0,0,0,0.5)',
     'rgba(0,0,0,0.4)',
     'rgba(0,0,0,0.1)',
     'rgba(0,0,0,0)',
--- src/redux/rootReducer.tsx
+++ src/redux/rootReducer.tsx
@@ -1,8 +1,14 @@
 import {combineReducers} from 'redux';
 import userSlice from './reducers/userSlice';
+import followingSlice from './reducers/followingSlice';
+import likeSlice from './reducers/likeSlice';
+import commentSlice from './reducers/commentSlice';
 
 const rootReducer = combineReducers({
   user: userSlice,
+  following: followingSlice,
+  like: likeSlice,
+  comment: commentSlice,
 });
 
 export default rootReducer;
